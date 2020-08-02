06da6f1a9fbf33b2be6b11cd8218d1377ae3f7a4
Monsur Ahmed Chowdhury <62332686+macrifat@users.noreply.github.com>
2020-08-03 01:52:29 +0200

    Update emailapp.java

--- emailapp.java
+++ emailapp.java
@@ -5,6 +5,9 @@ public class emailapp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       email em1 = new email("John", "Smith");
+		 em1.setChangePassword("mac@gmail.com");
+      System.out.println("Alternate email is: "+ em1.getChangePassword());
+	
 	}
 
 }

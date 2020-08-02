7f7f68f620222359642f7a05c20441dab0ad7b9d
Monsur Ahmed Chowdhury <62332686+macrifat@users.noreply.github.com>
2020-08-03 00:39:40 +0200

    Update email.java

--- email.java
+++ email.java
@@ -24,6 +24,8 @@ public class email {
 		System.out.print("Department : " +this.department);
 		
 		// call a methode that return random password
+		 this.password = randomPassword(8);
+		System.out.println("Your password is : "+ this.password);
 	}
 	
 	 
@@ -43,6 +45,12 @@ public class email {
 	private String randomPassword(int length) {
 		String setPassword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
 		char[] password = new char[length];
+		for (int i=0; i< length;i++) {
+			int rand = (int)(Math.random()* setPassword.length());
+			password[i]= setPassword.charAt(rand);
+		}
+		
+		return  new String(password);
 	}
 	
 	// set the mail capacity
